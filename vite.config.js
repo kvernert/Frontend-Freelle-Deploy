7d48c8d5e1a4e56b613c3cc6662d1c283e272eab
GabrielLima2803 <gabriellima2803@gmail.com>
2024-08-12 12:31:29 -0300

    fix: Importando o VitePWA

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 import vueDevTools from 'vite-plugin-vue-devtools'
+
 
 export default defineConfig({
   plugins: [
