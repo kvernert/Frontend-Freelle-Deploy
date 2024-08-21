8e887f8d411b8b5d4d691d92ea4f0e20581ecfb0
GabrielLima2803 <gabriellima2803@gmail.com>
2024-08-21 16:54:45 -0300

    feat: Adicionando os components no HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import { FooterComponent, HeaderComponent } from "@/components";
 </script>
 
 <template>
+  <HeaderComponent/>
   <main>
     Home
   </main>
+  <FooterComponent/>
 </template>
