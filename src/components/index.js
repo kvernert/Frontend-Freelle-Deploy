b541ab4405a6aaddc558c451e5b4514fc1e279e9
GabrielLima2803 <gabriellima2803@gmail.com>
2024-08-21 16:53:54 -0300

    feat: Adição do component Header no arquivo de exportação

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import FooterComponent from "@/components/MainComponents/footer/FooterComponent.vue";
+import HeaderComponent from "@/components/MainComponents/header/HeaderComponent.vue"
 
 export {    
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
 }
