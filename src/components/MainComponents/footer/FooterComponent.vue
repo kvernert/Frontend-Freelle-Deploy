701144f33ca1ff2b10891db14904cda5f1710875
GabrielLima2803 <gabriellima2803@gmail.com>
2024-08-21 16:53:37 -0300

    style: Corrigindo o css do Footer e do header

--- src/components/MainComponents/footer/FooterComponent.vue
+++ src/components/MainComponents/footer/FooterComponent.vue
@@ -1,15 +1,16 @@
 <script setup>
-
 </script>
 
 <template>
     <div class="container-footer">
         <div class="container-top">
             <div>
-            <h3 class="title">Freelle</h3>
+                <h3 class="title">Freelle</h3>
             </div>
             <div class="social">
-                
+                <i class="mdi mdi-facebook"></i>
+                <i class="mdi mdi-twitter"></i>
+                <i class="mdi mdi-linkedin"></i>
             </div>
         </div>
         <div class="row"></div>
@@ -20,40 +21,59 @@
 </template>
 
 <style scoped>
-.container-footer{
+.container-footer {
     position: fixed;
     left: 0;
     bottom: 0;
     width: 100%;
     font-family: "Inter", sans-serif;
 }
-.container-top{
+.container-top {
     position: relative;
     width: 75%;
     left: 50%;
     transform: translateX(-50%);
+    display: flex;
+    justify-content: space-between;
+    align-items: center; 
 }
-.title{
+
+.title {
     font-weight: bold;
     font-size: 26px;
+    padding-left: 15px;
 }
+.social{
+    padding-right: 15px;
+}
+
 .row {
     position: relative;
-    margin: 20px 0; 
+    margin: 15px 0;
 }
+
 .row::before {
     content: '';
     display: block;
     position: absolute;
     left: 50%;
     bottom: 0;
-    width: 75%; 
-    height: 2px; 
+    width: 75%;
+    height: 2px;
     background-color: black;
-    transform: translateX(-50%); 
+    transform: translateX(-50%);
 }
+
 .container-bottom {
-    text-align: center; 
-    padding: 10px 0; 
+    text-align: center;
+    padding: 10px 0;
+}
+.container-bottom > p {
+    font-size: 12px;
+}
+
+i {
+    font-size: 28px;
+    margin: 0 5px;
 }
 </style>
---
+++ src/components/MainComponents/header/HeaderComponent.vue
@@ -0,0 +1,56 @@
+<script setup>
+</script>
+
+<template>
+    <div class="header">
+  <div class="header-container">
+      <div class="header-left">
+        <img src="https://i.ibb.co/1KNDQpw/Freelee-icon.png" alt="" class="img">
+      </div>
+      <div class="header-right">
+        <button class="btn">Português</button>
+        <button class="btn">Entrar</button>
+        <button class="btn">Incrição</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header-container {
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto; 
+  padding: 0 1em;
+  display: flex;
+  justify-content: space-between;
+}
+
+.header {
+  width: 100%;
+  background-color: #006B63;
+  margin: 0;
+}
+.img{
+    width: 45px;
+}
+.header-right {
+  display: flex;
+  gap: 1.5em;
+}
+
+button {
+  border: none;
+  background: none;
+  padding: 0;
+  margin: 0;
+  outline: none;
+  box-shadow: none;
+  appearance: none;
+}
+
+.btn {
+  color: white;
+  cursor: pointer;
+}
+</style>
