6506d3e895a2b831fd45c89347b2a75a60461f5d
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-06 12:47:41 -0300

    feat: Criação do component de card user

---
+++ src/components/jobPage/CardUser.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="box-img">
+            <img src="" alt="">
+        </div>
+        <div class="bio">
+            
+        </div>
+        <div class="box-rating">
+
+        </div>
+        <div class="formation">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
