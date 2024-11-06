1ed16776ba7ffd3bf28a83445b0011303c11e51f
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-06 15:42:42 -0300

    feat: Criação da rota job-page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue';
 import LayoutFull from '@/layouts/LayoutFull.vue';
 import ServiceView from '@/views/ServiceView.vue';
 import LoginView from '@/views/LoginView.vue';
+import JobView from '@/views/JobView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +30,11 @@ const router = createRouter({
       name: 'Login',
       component: LoginView,
     },
+    {
+      path: '/job-page',
+      name: 'Job',
+      component: JobView,
+    }
   ],
 });
 
