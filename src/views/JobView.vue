c6ef04ccc3c98d424e95323cdbfc2d9477a53c4a
kvernert <kvernert47@gmail.com>
2024-11-07 00:05:50 -0300

    Responsividades

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,12 +1,30 @@
 <script setup>
-import { ContentMainJob, HeaderComponent, FooterComponent } from "@/components";
+import { ContentMainJob, HeaderComponent, FooterComponent, HeaderSmall, FooterSmall } from "@/components";
+import { ref, onMounted } from 'vue';
+const isSmallScreen = ref(false);
+
+const checkScreenSize = () => {
+  isSmallScreen.value = window.innerWidth <= 768;
+};
+
+onMounted(() => {
+  checkScreenSize();
+  window.addEventListener('resize', checkScreenSize);
+});
 </script>
 
 <template>
-    <HeaderComponent/>
-    <ContentMainJob/>
-    <FooterComponent/>
-</template>
+    <header-component v-if="!isSmallScreen" />
+    <header-small v-if="isSmallScreen" />
+    <div class="container">
+        <ContentMainJob/>
+    <div class="footer">
+    <footer-component v-if="!isSmallScreen" />
+    <footer-small v-if="isSmallScreen" />
+  </div>
+  </div>
+  </template>
+  
 
 <style scoped>
 
